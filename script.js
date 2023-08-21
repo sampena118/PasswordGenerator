7ac8d86a20212a50626cacc219425d4a7a359006
sampena118 <sampena118@icloud.com>
2023-08-21 10:10:32 -0700

    asking user how long they would like their password to be

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  var passwordLength = prompt("How long would you like your password to be? (8-128)")
+  if(passwordLength < 8 || passwordLength > 128) {
+    alert("Your password must have a length of between 8 and 128. Please retry!")
+  }
 
 }
 
